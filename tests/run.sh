d7377b2be3a39adbfcd6328e27d51f88417d6ef3
Syscore1 <32805386+Syscore1@users.noreply.github.com>
2023-01-20 11:49:28 -0700

    Update run.sh

--- tests/run.sh
+++ tests/run.sh
@@ -4,10 +4,12 @@ cd /
 cd var/www/html/MISECE-Repositorio
 sudo rm .env
 echo removed 
-sudo aws s3 cp s3://s3intercambio2/Re/.env /var/www/html/MISECE-Repositorio/ > /dev/null 2> /dev/null < /dev/null &
+sudo aws s3 cp s3://s3intercambio2/Re/.env /var/www/html/MISECE-Repositorio/
 sudo chown -R ec2-user ./composer.lock
 echo composerpreview
-sudo -u ec2-user composer update > s3://s3intercambio2/Re/logs/composer.txt
-php artisan migrate --force > s3://s3intercambio2/Re/logs/migrate.txt
+sudo -u ec2-user composer update > s3://s3intercambio2/Re/logs/composer.txt  > /dev/null 2> /dev/null < /dev/null &
+php artisan migrate --force > s3://s3intercambio2/Re/logs/migrate.txt > /dev/null 2> /dev/null < /dev/null &
+aws s3 cp composer.txt s3://s3intercambio2/Re/logs/
+aws s3 cp migrate.txt s3://s3intercambio2/Re/logs/
 echo artisan
 echo success!
