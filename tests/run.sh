1f4e49dd1ba907cff972b4d4a2787a04a6e0d5e1
Syscore1 <32805386+Syscore1@users.noreply.github.com>
2023-01-20 11:14:32 -0700

    update

---
+++ tests/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo yum update -y > /dev/null 2> /dev/null < /dev/null &
+cd /
+cd var/www/html/MISECE-Repositorio
+sudo rm .env
+echo removed 
+sudo aws s3 cp s3://s3intercambio2/Re/.env /var/www/html/MISECE-Repositorio/ > /dev/null 2> /dev/null < /dev/null &
+sudo chown -R ec2-user ./composer.lock
+echo composerpreview
+sudo -u ec2-user composer update > /dev/null 2> /dev/null < /dev/null &
+echo composer > composer.txt
+php artisan migrate --force > migrate.txt
+echo artisan
+echo success!
